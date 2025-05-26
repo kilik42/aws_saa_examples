eae0113894edbb4a3be35111db1870c15ee1407e
kilik42 <maevins@cps.edu>
2025-05-26 10:26:50 +0000

    working on ruby file

--- s3/sdk/ruby/s3.rb
+++ s3/sdk/ruby/s3.rb
@@ -31,5 +31,13 @@ number_of_files.times.each do |i|
   File.open(output_path, "w") do |f|
     f.write SecurerRandom.uuid
   end
+
+  # Upload file from disk in a single request, may not exceed 5GB
+  File.open('/source/file/path', 'rb') do |file|
+    s3.put_object(bucket: 'bucket-name', key: 'object-key', body: file)
+  end
+
+
+
 end
 
