e6097bb8fb49d37d868b1e99d8e8b222a140822c
kilik42 <maevins@cps.edu>
2025-05-26 10:23:43 +0000

    working on ruby file

--- s3/sdk/ruby/s3.rb
+++ s3/sdk/ruby/s3.rb
@@ -10,9 +10,24 @@ puts bucket_name
 client = Aws::S3::Client.new
 
 
+
 resp = client.create_bucket({
   bucket: bucket_name, 
   create_bucket_configuration: {
     location_constraint: region
   }, 
 })
+
+
+number_of_files = 1 + rand(6)
+
+puts "number_of_files: #{number_of_files}"
+
+number_of_files.times.each do |i|
+  puts "i : #{i}"
+  filename = "file_#{i}.txt"
+  output_path = "/tmp/#{filename}"
+
+  File.open
+end
+
