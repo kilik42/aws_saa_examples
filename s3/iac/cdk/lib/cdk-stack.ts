f3e09ddb0a66fab0a9848f85e218e9f69a664cd8
kilik42 <maevins@cps.edu>
2025-05-27 23:41:41 +0000

    cdk

---
+++ s3/iac/cdk/lib/cdk-stack.ts
@@ -0,0 +1,31 @@
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import *as s3 from 'aws-cdk-lib/aws-s3';
+import { RemovalPolicy } from 'aws-cdk-lib';
+
+import { S3 } from 'aws-cdk-lib/aws-ses-actions';
+// import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+   const bucket =  new s3.Bucket(scope, 'Bucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: true,
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
+
+    // const queue = new sqs.Queue(this, 'CdkQueue', {
+    //   visibilityTimeout: Duration.seconds(300)
+    // });
+
+    // const topic = new sns.Topic(this, 'CdkTopic');
+
+    // topic.addSubscription(new subs.SqsSubscription(queue));
+  }
+}
