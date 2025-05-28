b52d812d1cec7e23f52cdb94c315c1896f794b06
kilik42 <maevins@cps.edu>
2025-05-28 18:50:58 +0000

    adding terraform to etag folder

--- bin/terraform_cli_install.sh
+++ bin/terraform_cli_install.sh
@@ -1,6 +1,9 @@
   
   #!/usr/bin/env bash
-  sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+ - name: terraform
+    before: |
+     ./bin/terraform_cli_install.sh
+     sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
      wget -O- https://apt.releases.hashicorp.com/gpg | \
       gpg --dearmor | \
       sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
