70c0ef23f665c1c3bbf591edd859d0b026558143
kilik42 <maevins@cps.edu>
2025-06-18 21:36:42 +0000

    working on the encryption

--- s3/encryption-client/encrypt.rb
+++ s3/encryption-client/encrypt.rb
@@ -1,12 +1,16 @@
 require 'openssl'
+require 'aws-sdk-s3'
 key = OpenSSL::PKey::RSA.new(1024)
+
+bucket = 'encrypt-client-fun-ac-6534433'
+key = 'hello.txt'
 
 # encryption client
 s3 = Aws::S3::Encryption::Client.new(encryption_key: key)
 
 # round-trip an object, encrypted/decrypted locally
-s3.put_object(bucket:'aws-sdk', key:'secret', body:'handshake')
-s3.get_object(bucket:'aws-sdk', key:'secret').body.read
+s3.put_object(bucket:'aws-sdk', key:'hello.txt', body:'handshake')
+s3.get_object(bucket:'aws-sdk', key:'hello.txt').body.read
 #=> 'handshake'
 
 # reading encrypted object without the encryption client
